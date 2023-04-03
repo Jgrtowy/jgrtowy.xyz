237d424931fa5364f91582802d7a845318c32f4c
Dawid Gul <panjogurcik@outlook.com>
2023-04-03 17:34:32 +0200

    🔺 Changed to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import tailwind from "@astrojs/tailwind"
 import { defineConfig } from "astro/config"
 
-import netlify from "@astrojs/netlify/functions"
+import vercel from "@astrojs/vercel/serverless"
 
 // https://astro.build/config
 export default defineConfig({
      integrations: [tailwind()],
      output: "server",
-     adapter: netlify(),
+     adapter: vercel(),
 })
