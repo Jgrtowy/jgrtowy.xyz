477bac2cee5287884f2e040d0b58901c0f078037
Jgrtowy <panjogurcik@outlook.com>
2023-11-01 16:05:47 +0100

    💧 Minor bug fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,14 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
-export default defineConfig({
+export default /** @type {import('astro').AstroUserConfig} */ {
+    // your configuration options here...
+    // https://docs.astro.build/en/reference/configuration-reference/
     integrations: [tailwind()],
-    output: 'server',
+    output: 'hybrid',
     adapter: vercel(),
     redirects: {
         '/github': 'https://github.com/Jgrtowy',
         '/twitter': 'https://twitter.com/Jgrtowy',
     },
-});
+};
